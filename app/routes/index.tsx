863b2d2b62541db24185e1c332be720b070f054a
blakehartman <bhartman@clickrain.com>
2022-11-23 15:33:40 -0700

    replaced netlify build config with fly.io

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,4 @@
-import type {LinksFunction, LoaderArgs, LoaderFunction, MetaFunction} from '@remix-run/node'
+import type {LinksFunction, LoaderArgs, MetaFunction} from '@remix-run/node'
 import {json} from '@remix-run/node'
 import {Link, useLoaderData} from '@remix-run/react'
 import groq from 'groq'
